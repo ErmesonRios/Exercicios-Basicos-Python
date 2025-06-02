2f5ff3fd465ef25338410ddb4e8d5bdf268125ad
Ermeson Braga <red234red00@gmail.com>
2025-06-02 16:10:02 -0300

    update_6

--- tabuada.py
+++ tabuada.py
@@ -1,8 +1,11 @@
 import time
 
-multiplicador = int(input("Digite o número para ver a tabuada: "))
 
-for i in range(1, 11):
-    resultado = multiplicador * i
-    print(f"{multiplicador} x {i} = {resultado}")
-    time.sleep(0.5)
+while True:
+    multiplicador = int(input("Digite o número para ver a tabuada ou 0 para sair: "))
+    if multiplicador == 0:
+          break
+    for i in range(1, 11):
+        resultado = multiplicador * i
+        print(f"{multiplicador} x {i} = {resultado}")
+        time.sleep(0.5)
