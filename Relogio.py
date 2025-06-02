26ba368e3938e325837c4cfee9147bc56230da47
Ermeson Braga <red234red00@gmail.com>
2025-06-02 15:19:49 -0300

    update_3

--- Relogio.py
+++ Relogio.py
@@ -5,8 +5,13 @@ def relogio():
     while True:
         os.system('cls' if os.name == 'nt' else 'clear')
         hora = time.strftime("%H:%M:%S")
-        print(f"essa e a hora: {hora}")
+        data = time.strftime("%d/%m/%Y")
+        print("Relógio Atual:")
+        print(f"essa e a hora: {hora}: {data}")
         time.sleep(1)
+        if hora == "17:00:00":
+            print("Hora de ir embora!")
+            break
         
 if __name__ == "__main__":
     relogio()
