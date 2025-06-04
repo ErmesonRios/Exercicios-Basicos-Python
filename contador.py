3dc363b78a3896d89bdd74abd9b8d7fe5f2a8265
Ermeson Rios <red234red00@gmail.com>
2025-06-04 20:43:43 -0300

    PLUS

--- contador.py
+++ contador.py
@@ -1,14 +1,5 @@
 import time
 import os
 
-lista = []
-for i in range(1, 100):
-    lista.append(i)
-    time.sleep(0.1)
-for i in lista:
-    if i == 46:
-        print("Encontrei o número 46!")
-        break
-    
+for i in range(21, 0, -1):
     print(i)
-        
